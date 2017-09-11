e8418a111864fbe393afa206945af05974d3ffc7
Nick Paridon <paridonn1@gmail.com>
2017-09-11 15:56:16 -0400

    migration

--- db/migrate/20170911181144_create_responses.rb
+++ db/migrate/20170911181144_create_responses.rb
@@ -1,8 +1,7 @@
 class CreateResponses < ActiveRecord::Migration[5.1]
   def change
     create_table :responses do |t|
-      t.string :body 
-      t.timestamps
+      t.string :data
     end
   end
 end
